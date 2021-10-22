4446e9b4234a2cdb67d283406828f468bd2ac0c9
PaolaP <paolaperez8@gmail.com>
2021-10-22 00:34:00 +0000

    new notes day 2

---
+++ Week_1/Day_2/processargv.js
@@ -0,0 +1,8 @@
+'use strict';
+
+for (let j = 0; j < process.argv.length; j++) {
+  console.log(j + ' -> ' + (process.argv[j]));
+}
+
+//$ node processargv.js tom jack 43
+// line to run the code and get the indexes
