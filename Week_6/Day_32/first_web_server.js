3cb6719c89aa2be1b5df213104364d10a76dbd69
PaolaP <paolaperez8@gmail.com>
2021-11-22 15:50:49 -0500

    server gives dif results depending on the request path

--- Week_6/Day_32/first_web_server.js
+++ Week_6/Day_32/first_web_server.js
@@ -5,9 +5,14 @@ const PORT = 8080;
 
 // a function which handles requests and sends response
 const requestHandler = function (request, response) {
-	response.end(
-		`Requested Path: ${request.url}\nRequest Method: ${request.method}`
-	);
+	if (request.url === "/") {
+		response.end("Welcome!");
+	} else if (request.url === "/urls") {
+		response.end("www.lighthouselabs.ca\nwww.google.com");
+	} else {
+		response.statusCode = 404;
+		response.end("404 Page not found");
+	}
 };
 
 const server = http.createServer(requestHandler);
