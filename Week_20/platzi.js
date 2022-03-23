6d86df23d41a50ba53626f42958e09f1c228abd9
PaolaP <paolaperez8@gmail.com>
2022-03-22 23:07:27 -0400

    platzi basic

---
+++ Week_20/platzi.js
@@ -0,0 +1,7 @@
+let team1 = ['Oscar', 'Julian', 'Paola'];
+
+console.log(team1);
+//testing run code using extension
+
+//funciton expressiona re saved to a variable they can be anonympus
+//function declaration is just function(){};
