bf661f0531d3f4b514885e388635bb586b837b2f
PaolaP <paolaperez8@gmail.com>
2022-04-03 16:35:00 +0100

    exercise 35 branches and functions ruby

---
+++ Week_22_Ruby/exercise35.rb
@@ -0,0 +1,19 @@
+def gold_room
+  puts "This room is full of gold.  How much do you take?"
+
+  print "> "
+  choice = $stdin.gets.chomp
+   # this line has a bug, so fix it
+   if choice.include?("0") || choice.include?("1")
+    how_much = choice.to_i
+  else
+    dead("Man, learn to type a number.")
+  end
+
+  if how_much < 50
+    puts "Nice, you're not greedy, you win!"
+    exit(0)
+  else
+    dead("You greedy bastard!")
+  end
+end
