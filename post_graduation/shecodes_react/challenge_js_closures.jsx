d9d551d33af92ac77010e1093198da8c3ca01bb7
PaolaP <paolaperez8@gmail.com>
2022-06-02 15:59:52 -0400

    use arguments in the js closure

--- post_graduation/shecodes_react/challenge_js_closures.jsx
+++ post_graduation/shecodes_react/challenge_js_closures.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 function Weather(props) {
-  function fahrenheitTemperature() {
-    let Ftemperature = (props.temperature * 9) / 5 + 32;
+  function fahrenheitTemperature(celsius) {
+    let Ftemperature = (celsius * 9) / 5 + 32;
     let roundedFahrenheit = Math.round(Ftemperature);
     return roundedFahrenheit;
   }
   return (
     <div>
       The temperature in {props.city} is {props.temperature}°C and{" "}
-      {fahrenheitTemperature()}°F
+      {fahrenheitTemperature(props.temperature)}°F
     </div>
   );
 }
