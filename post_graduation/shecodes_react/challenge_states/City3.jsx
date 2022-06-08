c8f9d00d099b6c949e59f46eaf6e39c5f77a8b42
PaolaP <paolaperez8@gmail.com>
2022-06-07 22:50:11 -0400

    add metric and imperial state

--- post_graduation/shecodes_react/challenge_states/City3.jsx
+++ post_graduation/shecodes_react/challenge_states/City3.jsx
@@ -4,22 +4,24 @@ export default function City2(props) {
   //const { temperature, name } = props;
 
   let [temperature, setTemperature] = useState(props.temperature);
-
+  let [unit, setUnit] = useState("Metric");
   function clickFahrenheit(event) {
     event.preventDefault();
+    setUnit("Imperial");
     setTemperature(Math.round(props.temperature * 1.8 + 32));
     //alert(`${props.name} is ${Math.round(temperature)} F`);
   }
 
   function clickCelsius(event) {
     event.preventDefault();
+    setUnit("Metric");
     setTemperature(props.temperature);
     //alert(`${props.name} is ${Math.round(temperature)} F`);
   }
 
   return (
     <div className="City">
-      It is {temperature}{" "}
+      It is {temperature} ({unit})
       <a href="/" onClick={clickCelsius}>
         {" "}
         °C{" "}
