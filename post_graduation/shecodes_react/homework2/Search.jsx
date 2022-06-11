2167fbc9eda4705cde490c40897660603fe5fdfc
PaolaP <paolaperez8@gmail.com>
2022-06-11 01:00:22 -0400

    hmw2

---
+++ post_graduation/shecodes_react/homework2/Search.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export default function Search() {
+  const [city, setCity] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`It is 19 degrees in ${city}`);
+  }
+
+  function changeOutput(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Search for a city"
+        autoFocus={true}
+        onChange={changeOutput}
+      />
+
+      <input type="submit" value="Search" />
+    </form>
+  );
+}
