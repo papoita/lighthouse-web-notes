1a3c14a9b1620971b91ec8c3570b2de3f14de949
PaolaP <paolaperez8@gmail.com>
2022-06-12 15:46:00 -0400

    react loops data inside component

---
+++ post_graduation/shecodes_react/react_loops/Cities.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+export default function Cities() {
+  let cities = ["Paris", "Tokyo", "Ottawa"];
+
+  return (
+    <div className="Cities">
+      <ul>
+        {cities.map(function (city, index) {
+          return <li key={index}>{city}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+/// using arrow functions
+import React from "react";
+
+export default function Cities() {
+  let cities = ["Paris", "Tokyo", "Ottawa"];
+
+  return (
+    <div className="Cities">
+      <ul>
+        {cities.map((city, index)=>{
+          return <li key={index}>{city}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+////array of objects
+
+import React from "react";
+
+export default function Cities() {
+  let cities = [
+    {
+      name: "paris",
+      temp: 12,
+    },
+    {
+      name: "tokyo",
+      temp: 30,
+    }
+  ]
+
+  return (
+    <div className="Cities">
+      <ul>
+        {cities.map((city, index)=>{
+          return <li key={index}>It is{city.temp} degrees in <strong>{city.name}</strong></li>;
+        })}
+      </ul>
+    </div>
+  );
+}
