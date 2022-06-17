c6b329a82a1c5d981c5f08bb11c2084de94bd8a2
PaolaP <paolaperez8@gmail.com>
2022-06-16 22:11:26 -0400

    loader using npm react-loader-spinner

--- post_graduation/shecodes_react/ajax/Weather.jsx
+++ post_graduation/shecodes_react/ajax/Weather.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
-//const axios = require("axios");
+import Loader from "react-loader-spinner";
+
 
 export default function Weather(props) {
   let [temperature, setTemperature] = useState(null);
@@ -10,17 +11,32 @@ export default function Weather(props) {
     setTemperature(response.data.main.temp);
   }
 
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=3a5d0107b87d2358462582e10139dd2d&units=metric`;
+ 
 
-  axios.get(url).then(showTemperature);
-  
   if (temperature) {
     return (
+      <>
+       <Loader
+          type="ThreeDots"
+          color="red"
+          height={110}
+          width={110}
+          ariaLabel="three-circles-rotating"
+        />
+      
       <p>
         The weather is {props.city} is {Math.round(temperature)}
       </p>
+      </>
     );
   } else {
-    return <p>Loading temperature for {props.city}...</p>;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=3a5d0107b87d2358462582e10139dd2d&units=metric`;
+
+    axios.get(url).then(showTemperature);
+
+    return <>
+    <Loader type="ThreeDots" color="black" height={50} width={50} />;
+    <p>Loading temperature for {props.city}...</p>;
+    </>
   }
 }
